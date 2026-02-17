440ca521790e7489f99a856cd8d5369dc299d336
nyayath lual <n.chol@alustudent.com>
2026-02-17 15:48:27 +0000

    gathering data from an api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Gather data from an API for a given employee ID and display
-their TODO list progress.
+Gather data from an API for a given employee ID.
 """
 
 import requests
@@ -9,37 +8,40 @@ import sys
 
 
 if __name__ == "__main__":
-    # Check if employee ID is provided
-    if len(sys.argv) != 2:
-        sys.exit(1)
-
     employee_id = sys.argv[1]
 
-    # API endpoints
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    user_url = (
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(employee_id)
+    )
+    todos_url = (
+        "https://jsonplaceholder.typicode.com/users/{}/todos"
+        .format(employee_id)
+    )
 
-    # Fetch user information
     user_response = requests.get(user_url)
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
-    # Fetch TODO list
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
-    # Calculate completed tasks
     total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get("completed")]
+
+    done_tasks = [
+        task for task in todos_data
+        if task.get("completed") is True
+    ]
+
     number_done_tasks = len(done_tasks)
 
-    # Print progress
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name,
-        number_done_tasks,
-        total_tasks
-    ))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name,
+            number_done_tasks,
+            total_tasks
+        )
+    )
 
-    # Print completed task titles
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
