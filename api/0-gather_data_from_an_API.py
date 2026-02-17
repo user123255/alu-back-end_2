4dd9e5889f845d2bb384b394b81741571a19a11b
nyayath lual <n.chol@alustudent.com>
2026-02-17 20:24:18 +0000

    gathering data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,33 +4,39 @@ Gather data from an API for a given employee ID and display
 their TODO list progress.
 """
 
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
 
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    user_url = (
+        f"https://jsonplaceholder.typicode.com/users/"
+        f"{employee_id}"
+    )
 
-    # Fetch user data
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/users/"
+        f"{employee_id}/todos"
+    )
+
     user_response = requests.get(user_url)
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
-    # Fetch todos data
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
     total_tasks = len(todos_data)
 
     done_tasks = [
-        task for task in todos_data if task.get("completed") is True
+        task for task in todos_data
+        if task.get("completed") is True
     ]
+
     number_done_tasks = len(done_tasks)
 
-    # Display employee task completion
     print(
         f"Employee {employee_name} is done with tasks"
         f"({number_done_tasks}/{total_tasks}):"
